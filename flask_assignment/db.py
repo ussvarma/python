d614510bba34fc121923cde7a661b7e4528ed126
ussvarma <saivarmauddaraju@gmail.com>
2022-03-31 10:44:24 +0530

    assignment

--- flask_assignment/db.py
+++ flask_assignment/db.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-conn=sqlite3.connect("books.sqlite")
+conn = sqlite3.connect("books.sqlite")  # for establishing connection with database
 
-cursor=conn.cursor()
-sql_query="""CREATE TABLE book(
+cursor = conn.cursor()  # for creating table
+sql_query = """CREATE TABLE book(
       id integer PRIMARY KEY,
       author text NOT NULL,
       language text NOT NULL,
