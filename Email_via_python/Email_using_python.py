c8efbbd2347081ac78f0350808c07f2e823fe9c0
ussvarma <saivarmauddaraju@gmail.com>
2022-04-01 19:30:50 +0530

    assignment

--- Email_via_python/Email_using_python.py
+++ Email_via_python/Email_using_python.py
@@ -1,28 +1,38 @@
 import smtplib
 from configparser import ConfigParser
 
-file = "config.ini"
-config = ConfigParser()
-config.read(file)
 
-to_address = config["email"]["to"]
-cc_list = config["email"]["cc"]
-bcc_list = config["email"]["bcc"]
-from_address = config["email"]["from"]
 
-message_subject = config["email"]["subject"]
-message_text = config["email"]["text"]
-message = "From: %s\r\n" % from_address \
-          + "To: %s\r\n" % to_address \
-          + "CC: %s\r\n" % ",".join(cc_list) \
-          + "Bcc: %s\r\n" % ",".join(bcc_list) \
-          + "Subject: %s\r\n" % message_subject \
-          + "\r\n" + message_text
 
-to_addresses_list = [to_address] + cc_list + bcc_list
-server = smtplib.SMTP('smtp.gmail.com')
-server.starttls()
-server.login(config["login"]["user_name"], config["login"]["password"])
-server.set_debuglevel(1)
-server.sendmail(from_address, to_addresses_list, message)
-server.quit()
+def Main():
+    file = "config.ini"
+    config = ConfigParser()
+    config.read(file)
+
+    to_address = config["email"]["to"]
+    cc_list = [config["email"]["cc"]]
+    bcc_list = [config["email"]["bcc"]]
+    from_address = config["email"]["from"]
+
+
+
+    message_subject = config["email"]["subject"]
+    message_text = config["email"]["text"]
+    message = "From: %s\r\n" % from_address \
+              + "To: %s\r\n" % to_address \
+              + "CC: %s\r\n" % ",".join(cc_list) \
+              + "Bcc: %s\r\n" % ",".join(bcc_list) \
+              + "Subject: %s\r\n" % message_subject \
+              + "\r\n" + message_text
+
+    to_addresses_list = [to_address] + cc_list + bcc_list
+    server = smtplib.SMTP('smtp.gmail.com')      #simple mail transfer protocol
+    server.starttls()                            #for making secure connection
+    server.login(config["login"]["user_name"], config["login"]["password"])
+    server.set_debuglevel(1)
+    server.sendmail(from_address, to_addresses_list, message)
+    server.quit()
+
+
+if __name__ == '__main__':
+    Main()
