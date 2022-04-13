d850168d56b3a4393a4a5a98fb4af6b5362de59e
ussvarma <saivarmauddaraju@gmail.com>
2022-04-13 09:24:12 +0530

    assignment

--- flask_assignment/app.py
+++ flask_assignment/app.py
@@ -1,10 +1,11 @@
+# importing libraries
 import sqlite3
-
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
+# connection with database
 def db_connection():
     conn = None
     try:
@@ -19,23 +20,24 @@ def books():
     conn = db_connection()
     conn.cursor()
 
-    if request.method == "GET":
-        cursor = conn.execute("SELECT * FROM book")
+    if request.method == "GET":  # retrieves information
+        cursor = conn.execute("SELECT * FROM book")  # query
         books = []
         for row in cursor.fetchall():
             books.append(dict(id=row[0], author=row[1], language=row[2], title=row[3]))
         if books is not None:
             return jsonify(books)
 
-    if request.method == "POST":
+    if request.method == "POST":  # for adding new data
         new_author = request.form['author']
         new_lang = request.form['language']
         new_title = request.form['title']
-        sql = """INSERT INTO book (author, language,title)
+        # query for inserting a row  into table
+        sql = """INSERT INTO book (author, language,title) 
             VALUES (?,?,?)
         """
         cursor = conn.execute(sql, (new_author, new_lang, new_title))
-        conn.commit()
+        conn.commit()  # saving the changes
         return f"Book with the id: {cursor.lastrowid} created successfully"
 
 
@@ -50,11 +52,11 @@ def single_book(id):
         for r in rows:
             book = r
         if book is not None:
-            return jsonify(book), 200
+            return jsonify(book), 200  # indicates request has been succeeded
         else:
-            return "Something wrong", 404
+            return "Something wrong", 404  # the server cannot find the requested resource.
 
-    if request.method == "PUT":
+    if request.method == "PUT":  # for updating existing data
         sql = """UPDATE book SET 
                 title=?,
                 author=?,
@@ -73,7 +75,7 @@ def single_book(id):
         conn.commit()
         return jsonify(updated_book)
 
-    if request.method == "DELETE":
+    if request.method == "DELETE":  #for deleting existing data
         sql = """ DELETE FROM book  WHERE id=?"""
         conn.execute(sql, (id,))
         conn.commit()
