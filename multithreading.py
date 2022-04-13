e29dfa467168fdb3af2df1afa24cb17296af8045
ussvarma <saivarmauddaraju@gmail.com>
2022-04-13 09:03:54 +0530

    assignment

--- multithreading.py
+++ multithreading.py
@@ -1,3 +1,4 @@
+# A simple code to explain multithreading
 import threading
 
 
@@ -9,12 +10,12 @@ def print_square(num):
     print(num ** 2)
 
 
-t1 = threading.Thread(target=print_cube, args=(10,))
-t2 = threading.Thread(target=print_square, args=(10,))
+t1 = threading.Thread(target=print_cube, args=(10,))  # defining one thread
+t2 = threading.Thread(target=print_square, args=(10,)) # defining second thread
 t1.start()
 t2.start()
+
+# waits until both threads finish their task
 t1.join()
 t2.join()
 print("Done")
-
-
